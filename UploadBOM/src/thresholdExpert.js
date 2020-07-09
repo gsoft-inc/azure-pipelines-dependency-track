0ce74c63e99a69292ca6fd4389f75f8c251f3d96
Edouard <Zargath>
2020-07-08 22:58:15 -0400

    CleanUp

--- UploadBOM/src/thresholdExpert.js
+++ UploadBOM/src/thresholdExpert.js
@@ -3,23 +3,23 @@ import { localize } from "./localization";
 class ThresholdExpert {
   constructor(criticalCount, highCount, mediumCount, lowCount, unassignedCount) {
     if (!Number.isInteger(criticalCount)) {
-      throw new Error(localize('ThresholdInNotAnInteger', 'Critical'));
+      throw new Error(localize('ThresholdInNotAnInteger', localize('Critical')));
     }
 
     if (!Number.isInteger(highCount)) {
-      throw new Error(localize('ThresholdInNotAnInteger', 'High'));
+      throw new Error(localize('ThresholdInNotAnInteger', localize('High')));
     }
 
     if (!Number.isInteger(mediumCount)) {
-      throw new Error(localize('ThresholdInNotAnInteger', 'Medium'));
+      throw new Error(localize('ThresholdInNotAnInteger', localize('Medium')));
     }
 
     if (!Number.isInteger(lowCount)) {
-      throw new Error(localize('ThresholdInNotAnInteger', 'Low'));
+      throw new Error(localize('ThresholdInNotAnInteger', localize('Low')));
     }
 
     if (!Number.isInteger(unassignedCount)) {
-      throw new Error(localize('ThresholdInNotAnInteger', 'Unassigned'));
+      throw new Error(localize('ThresholdInNotAnInteger', localize('Unassigned')));
     }
 
     this.criticalCount = criticalCount;
@@ -39,23 +39,23 @@ class ThresholdExpert {
 
   validateThresholds(metrics) {
     if (this.criticalCount >= 0 && metrics.critical > this.criticalCount) {
-      throw new Error(localize('ThresholdSurpassed', 'Critical vulnerability count'));
+      throw new Error(localize('VulnCountThresholdSurpassed', localize('Critical')));
     }
 
     if (this.highCount >= 0 && metrics.high > this.highCount) {
-      throw new Error(localize('ThresholdSurpassed', 'High vulnerability count'));
+      throw new Error(localize('VulnCountThresholdSurpassed', localize('High')));
     }
 
     if (this.mediumCount >= 0 && metrics.medium > this.mediumCount) {
-      throw new Error(localize('ThresholdSurpassed', 'Medium vulnerability count'));
+      throw new Error(localize('VulnCountThresholdSurpassed', localize('Medium')));
     }
 
     if (this.lowCount >= 0 && metrics.low > this.lowCount) {
-      throw new Error(localize('ThresholdSurpassed', 'Low vulnerability count'));
+      throw new Error(localize('VulnCountThresholdSurpassed', localize('Low')));
     }
 
     if (this.unassignedCount >= 0 && metrics.unassigned > this.unassignedCount) {
-      throw new Error(localize('ThresholdSurpassed', 'Unassigned vulnerability count'));
+      throw new Error(localize('VulnCountThresholdSurpassed', localize('Unassigned')));
     }
   }
 }
